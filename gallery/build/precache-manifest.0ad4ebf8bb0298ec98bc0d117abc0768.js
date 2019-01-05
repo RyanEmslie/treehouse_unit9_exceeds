dc00f70b3796dda9392a2dc5945c3d5cfabad429
RyanEmslie <ryan.emslie@gmail.com>
2019-01-05 10:54:06 -0500

    initial app submit

---
+++ gallery/build/precache-manifest.0ad4ebf8bb0298ec98bc0d117abc0768.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe183b847c8edf89eba9dc073b0b3f5d",
+    "url": "/static/media/404.fe183b84.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "416f73de92508d5b9090",
+    "url": "/static/js/main.416f73de.chunk.js"
+  },
+  {
+    "revision": "118bcf2855f583cbe588",
+    "url": "/static/js/1.118bcf28.chunk.js"
+  },
+  {
+    "revision": "416f73de92508d5b9090",
+    "url": "/static/css/main.42994e12.chunk.css"
+  },
+  {
+    "revision": "a16d9070282601a2b1adb319fd992261",
+    "url": "/index.html"
+  }
+];
